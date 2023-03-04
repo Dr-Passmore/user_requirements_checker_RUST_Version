123d36f6d9a3acf3eefc93971a71b3ee6852980b
Phillip Passmore <phillipspassmore@gmail.com>
2023-03-04 14:03:28 +0000

    splits each item by line then whitespace
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::collections::HashSet;
 fn main() {
     // Initial list creation to compare content of 'userstories used.txt'
     let mut list_of_user_stories = HashSet::new();
-    //let mut used_items = HashSet::new();
+    let mut used_items = HashSet::new();
     let mut requirements = 0;
 
     // Creates the user requirements list in the same string format - example "US001"
@@ -13,9 +13,19 @@ fn main() {
         // Add each to list of user stories
         list_of_user_stories.insert(us);
     }
-
+    // Takes data from the userstories used.txt file
     let data = fs::read_to_string("userstories used.txt").expect("Failed to read file");
+    // Processes data initally line by line
+    for line in data.lines() {
+        let values: Vec<String> = line.split_whitespace().map(|s| s.to_string()).collect();
+        requirements += 1;
+        for j in values {
+            // for each item found it is appended to list
+            used_items.insert(j);
+        }
+    }
 
 
-   println!("{}",data)
+   println!("{}",data);
+   println!("{}", requirements);
 }
